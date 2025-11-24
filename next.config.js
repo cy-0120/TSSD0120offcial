3ccb90f4e37af291d523798f9639948b59f637da
TSSD0120 <dragon63503514@gmail.com>
2025-11-24 11:57:01 +0900

    방문자 활성화

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   // 환경 변수 설정
   env: {
     CUSTOM_KEY: process.env.CUSTOM_KEY,
-    // NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || '주소',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://tssd0120.kro.kr/main',
   },
 }
 
